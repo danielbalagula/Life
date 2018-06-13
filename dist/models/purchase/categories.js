a0774fd05c0d572093464273f38b83b58e4ace4a
Daniel Balagula <danielbalagula@Daniels-iMac.local>
2018-06-13 16:32:55 -0500

    remove dist

--- dist/models/purchase/categories.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = ['video games', 'breakfast', 'lunch', 'dinner', 'snacks', 'bars', 'books', 'music', 'movies', 'transportation', 'technology', 'supplies', 'clothing'];
-//# sourceMappingURL=../../src/models/purchase/categories.js.map
